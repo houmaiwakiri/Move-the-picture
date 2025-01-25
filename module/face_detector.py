db9fe6c86c07c5d59cf8e8a8abf3a8f4d5de0224
houmaiwakiri <mahomahoma888@gmail.com>
2025-01-25 18:26:56 +0900

    コメントアウト修正

--- module/face_detector.py
+++ module/face_detector.py
@@ -10,15 +10,12 @@ def start_face_detection():
     # 顔検出のためのカスケード分類器を読み込む
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    # Pygameを初期化
     pygame.init()
-
-    # ウィンドウサイズ設定
     screen = pygame.display.set_mode((640, 480))
-    pygame.display.set_caption("Move Image with Webcam")
+    pygame.display.set_caption("喜怒哀楽")
 
     # 画像を読み込む
-    base_dir = os.path.dirname(os.path.abspath(__file__))  # 現在のファイルの絶対パス
+    base_dir = os.path.dirname(os.path.abspath(__file__))
     images = {
         "happy": load_image(os.path.join(base_dir, 'img', 'happy.png')),
         "sad": load_image(os.path.join(base_dir, 'img', 'sad.png')),
@@ -31,8 +28,8 @@ def start_face_detection():
     # カメラを開く
     cap = cv2.VideoCapture(0)
 
+    # 常に読み込み続けるために無限ループ
     while True:
-        # Pygameのイベント処理
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 cap.release()
@@ -46,34 +43,32 @@ def start_face_detection():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-        # 表情認識をDeepFaceで行う
+        # 表情認識
         try:
-            # DeepFaceで表情認識を行う
-            analysis = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
-            # 最も強い感情を取得
+            #感情を取得し、認識できない場合エラー
+            analysis = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=True)
             emotion = analysis[0]['dominant_emotion']
-        except Exception as e:
-            print("Error in emotion analysis:", e)
-            emotion = "neutral"  # エラー時はデフォルトでneutral
+        except Exception:
+            # エラー時は真顔
+            emotion = "neutral"
 
-        # 画像を更新
+        # 画像更新
         image = images.get(emotion, images["neutral"])
         image_rect = image.get_rect()
 
-        # 顔が検出されたら、その位置に画像を移動
+        # 顔検出位置に画像を移動
         for (x, y, w, h) in faces:
             image_rect.center = (x + w // 2, y + h // 2)
 
-        # ウィンドウを塗りつぶす
+        #背景を黒に
         screen.fill((0, 0, 0))
 
-        # 画像を描画
+        # 画像表示
         screen.blit(image, image_rect)
 
-        # 画面を更新
+        # 画面更新
         pygame.display.flip()
 
-        # 'q'キーで終了
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
